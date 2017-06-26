8fadec2fcd4ec53e58dc6036393a2375fbe495a1
torbee <vkishman88@gmail.com>
2017-06-26 18:13:28 -0400

    Add files via upload

--- Lab1.cs
+++ Lab1.cs
@@ -11,21 +11,26 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            string input3 = "y";
+            while (input3 == "y")
+            {
+                Console.WriteLine("Enter Length: ");
+                string input1 = Console.ReadLine();
+                float length = float.Parse(input1);
 
-            Console.WriteLine("Enter Length: ");
-            string input1 = Console.ReadLine();
-            float length = float.Parse(input1);
+                Console.WriteLine("Enter Width: ");
+                string input2 = Console.ReadLine();
+                float width = float.Parse(input2);
 
-            Console.WriteLine("Enter Width: ");
-            string input2 = Console.ReadLine();
-            float width = float.Parse(input2);
+                float area = length * width;
+                Console.WriteLine("Area: " + area);
 
-            float area = length * width;
-            Console.WriteLine("Area: "+ area);
+                float perimeter = (length * 2) + (width * 2);
+                Console.WriteLine("Perimeter: " + perimeter);
 
-            float perimeter = (length * 2) + (width * 2);
-            Console.WriteLine("Perimeter: " + perimeter);
-
+                Console.WriteLine("Continue? Y/N:");
+                input3 = Console.ReadLine();
+            }
         }
     }
 }
