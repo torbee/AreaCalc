983ead365532f5dbb91937397ab743153feddd64
torbee <vkishman88@gmail.com>
2017-06-26 15:41:13 -0400

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+            Console.WriteLine("Enter Length: ");
+            string input1 = Console.ReadLine();
+            float length = float.Parse(input1);
+
+            Console.WriteLine("Enter Width: ");
+            string input2 = Console.ReadLine();
+            float width = float.Parse(input2);
+
+            float area = length * width;
+            Console.WriteLine("Area: "+ area);
+
+            float perimeter = (length * 2) + (width * 2);
+            Console.WriteLine("Perimeter: " + perimeter);
+
+        }
+    }
+}
